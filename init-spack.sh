969f04a6c2c8a375a845fd749b82b22fdd5ca2e7
Lawrence root <root@head.cluster>
2017-09-28 15:51:10 -0500

    works better with intel compiler, uses default config file for compilers.yaml

--- init-spack.sh
+++ init-spack.sh
@@ -18,10 +18,26 @@ source $SPACK_ROOT/share/spack/setup-env.sh
 echo
 echo "Adding intel compiler"
 module load intel
-spack compiler find
+
+# Create /apps/spack/etc/spack/defaults/compilers.yaml
+spack compiler find --scope defaults
+
 # Prefer intel over gcc over pgi
 sed -i 's/^    compiler: \[gcc, intel, pgi, clang, xl, nag\]/    compiler: \[intel, gcc, pgi, clang, xl, nag\]/g' \
 	$SPACK_ROOT/etc/spack/defaults/packages.yaml
+
+# add intel optimizations and library rpaths to config
+COMPILERS="$SPACK_ROOT/etc/spack/defaults/compilers.yaml"
+
+tac $SPACK_ROOT/etc/spack/defaults/compilers.yaml | sed -e '0,/extra_rpaths: \[\]/s//     - \/opt\/intel\/compilers_and_libraries_2017.4.196\/linux\/compiler\/lib\/intel64 \
+    extra_rpaths: \ /'|tac >${COMPILERS}.new && mv ${COMPILERS}.new $COMPILERS
+
+tac $COMPILERS | sed -e '0,/flags: {}/s//flags: \
+      cflags: -O3 \
+      cxxflags: -O3 \
+      cppflags: -O3 \
+      fflags: -O3 \
+      fcflags: -O3 /' | tac >${COMPILERS}.new && mv ${COMPILERS}.new $COMPILERS
 
 echo
 echo "Configuring Modules"
@@ -33,7 +49,8 @@ sed -i "/^modules:/a\
        autoload: \'all\' \n" \
         $SPACK_ROOT/etc/spack/defaults/modules.yaml
 
-# echo "build_jobs: 20" >> $SPACK_ROOT/etc/spack/defaults/config.yaml
+#echo "build_jobs: 20" >> $SPACK_ROOT/etc/spack/defaults/config.yaml
+
 
 echo
 echo "Bootstrapping"
