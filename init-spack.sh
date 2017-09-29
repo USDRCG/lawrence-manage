bf20cbae21c0ce50f54d1012baf53822deece32a
Lawrence root <root@head.cluster>
2017-09-29 11:53:34 -0500

    Separated out config, added update scripts

--- init-spack.sh
+++ init-spack.sh
@@ -15,43 +15,7 @@ git clone $SPACK_REPO $SPACK_ROOT
 
 source $SPACK_ROOT/share/spack/setup-env.sh
 
-echo
-echo "Adding intel compiler"
-module load intel
-
-# Create /apps/spack/etc/spack/defaults/compilers.yaml
-spack compiler find --scope defaults
-
-# Prefer intel over gcc over pgi
-sed -i 's/^    compiler: \[gcc, intel, pgi, clang, xl, nag\]/    compiler: \[intel, gcc, pgi, clang, xl, nag\]/g' \
-	$SPACK_ROOT/etc/spack/defaults/packages.yaml
-
-# add intel optimizations and library rpaths to config
-COMPILERS="$SPACK_ROOT/etc/spack/defaults/compilers.yaml"
-
-tac $SPACK_ROOT/etc/spack/defaults/compilers.yaml | sed -e '0,/extra_rpaths: \[\]/s//     - \/opt\/intel\/compilers_and_libraries_2017.4.196\/linux\/compiler\/lib\/intel64 \
-    extra_rpaths: \ /'|tac >${COMPILERS}.new && mv ${COMPILERS}.new $COMPILERS
-
-tac $COMPILERS | sed -e '0,/flags: {}/s//\
-      cflags: -O3 \
-      cxxflags: -O3 \
-      cppflags: -O3 \
-      fflags: -O3 \
-      fcflags: -O3 \
-    flags:/' | tac >${COMPILERS}.new && mv ${COMPILERS}.new $COMPILERS
-
-echo
-echo "Configuring Modules"
-sed -i "/^modules:/a\
-\ \ tcl: \n \
-    hash_length: 0 \n \
-    naming_scheme: '\${PACKAGE}/\${VERSION}-\${COMPILERNAME}-\${COMPILERVER}' \n \
-    ^r: \n \
-       autoload: \'all\' \n" \
-        $SPACK_ROOT/etc/spack/defaults/modules.yaml
-
-#echo "build_jobs: 20" >> $SPACK_ROOT/etc/spack/defaults/config.yaml
-
+./config-spack.sh
 
 echo
 echo "Bootstrapping"
